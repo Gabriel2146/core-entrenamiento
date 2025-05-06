7913fc0dc2fdf8fb952a2d802b3ed2e241dd1a25
Gabriel2146 <marcelo.padilla.rodriguez@udla.edu.ec>
2025-05-06 15:10:40 -0500

    Crear estructura por apps

--- backend/settings.py
+++ backend/settings.py
@@ -39,9 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'usuarios',
-    'entrenamiento',
-    'localizacion',
+    #'usuarios',
+    #'entrenamiento',
+    #'localizacion',
 ]
 
 MIDDLEWARE = [
